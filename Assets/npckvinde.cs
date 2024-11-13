12f4340f8934716be3e7ff0339ebad6c3d2359db
HaseebTahir0054 <mian.m.haseeb0054@gmail.com>
2024-11-13 21:52:02 +0100

    Spil done

--- Assets/npckvinde.cs
+++ Assets/npckvinde.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
 
-public class npckvinde : MonoBehaviour
+public class npcmand : MonoBehaviour
 {
     public NPCConversation myConversation;
 
