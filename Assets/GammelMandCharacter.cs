edb24898edf2c20528eb66f1af352fd9f08ad2f0
Viros2p <arma0096x@gmail.com>
2024-11-14 15:35:30 +0100

    fikset hele spillet

--- Assets/GammelMandCharacter.cs
+++ Assets/GammelMandCharacter.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
 
-public class kvinde : MonoBehaviour
+public class npcmand : MonoBehaviour
 {
     public NPCConversation myConversation;
-    //bool conversationStarted;
-
-   // public Transform player;
-   // public float distanceToStartConversation = 5;
+    //bool conversationStarted = false;
+    //public Transform player;
+    //public float distanceToStartConversation = 5;
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // && !conversationStarted && Vector3.Distance(transform.position, player.position) < distanceToStartConversation)
+        if (Input.GetMouseButtonDown(0)) //&& !conversationStarted && Vector3.Distance(transform.position, player.position) < distanceToStartConversation)
         {
             //conversationStarted = true;
             ConversationManager.Instance.StartConversation(myConversation);
--- Assets/GammelMandConversation.cs
+++ Assets/GammelMandConversation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Farligmandsamatale : MonoBehaviour
+public class GammelMandConversation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
